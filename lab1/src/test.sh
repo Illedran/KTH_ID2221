79223590e3a115ddb8f7cb07349553a7c312e3b1
Illedran <nrd.nardelli@gmail.com>
2018-09-20 18:30:39 +0200

    Added bash helper to compile&run

---
+++ lab1/src/test.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+rm -rf topten_classes
+mkdir -p topten_classes
+javac -cp $HADOOP_CLASSPATH -d topten_classes topten/TopTen.java
+jar -cvf topten.jar -C topten_classes/ .
+hadoop jar topten.jar topten.TopTen topten_input topten_output
