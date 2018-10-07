0971eec9f8dab10efb4be038c7bdf525f715ac20
Illedran <nrd.nardelli@gmail.com>
2018-10-07 16:48:43 +0200

    Indent + classname

--- lab3/src/sparkstreaming/KafkaSpark.scala
+++ lab3/src/sparkstreaming/KafkaSpark.scala
@@ -20,12 +20,12 @@ import com.datastax.spark.connector.streaming._
 
 
 // define a class to store the visiting state of each key-value pair
-case class VisitState(count: Int = 0, sum: Double = 0.0) {
+case class MyState(count: Int = 0, sum: Double = 0.0) {
   // stores the average visit count
   val avg = sum / scala.math.max(count, 1)
 
   // define the add operator of the class
-  def +(count: Int, sum: Double) = VisitState (
+  def +(count: Int, sum: Double) = MyState (
     this.count + count,
     this.sum + sum
   )
@@ -38,7 +38,7 @@ case class VisitState(count: Int = 0, sum: Double = 0.0) {
 
 object KafkaSpark {
   def main(args: Array[String]) {
-    
+
     // connect to Cassandra and make a keyspace and table as explained in the document
 
     val cluster = Cluster.builder().addContactPoint("127.0.0.1").build()
@@ -51,36 +51,36 @@ object KafkaSpark {
     // make a connection to Kafka and read (key, value) pairs from it
     // connect Spark to Kafka in the receiver-less direct approach where Spark periodically queries Kafka for the latest offsets in each topic + partition
 
-	val sparkConf = new SparkConf().setAppName("KafkaSparkAverageValue").setMaster("local[2]")
-	val ssc = new StreamingContext(sparkConf, Seconds(1))
+    val sparkConf = new SparkConf().setAppName("KafkaSparkAverageValue").setMaster("local[2]")
+    val ssc = new StreamingContext(sparkConf, Seconds(1))
     val kafkaConf = Map(
-    	"metadata.broker.list" -> "localhost:9092",
-    	"zookeeper.connect" -> "localhost:2181",
-    	"group.id" -> "kafka-spark-streaming",
-    	"zookeeper.connection.timeout.ms" -> "1000"
+      "metadata.broker.list" -> "localhost:9092",
+      "zookeeper.connect" -> "localhost:2181",
+      "group.id" -> "kafka-spark-streaming",
+      "zookeeper.connection.timeout.ms" -> "1000"
     )
 
     val topicsSet = Set("avg")
 
     val kafkaStream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
-    	ssc, kafkaConf, topicsSet)
+      ssc, kafkaConf, topicsSet)
 
     // extract useful information as the origianl key-value pair are of the format (null, "a, 10")
-	val lines = kafkaStream.map(x => x._2)
-	val arr = lines.map(x => x.split(","))
-	val pairs = arr.map(record => (record(0), record(1).toDouble))
- 
+    val lines = kafkaStream.map(x => x._2)
+    val arr = lines.map(x => x.split(","))
+    val pairs = arr.map(record => (record(0), record(1).toDouble))
 
- 	// define the maping function
- 	// State[Object] will retain its value across different mini batch
-    def mappingFunc(key: String, value: Option[Double], state: State[VisitState]): (String, Double) = {
-    	val delta = value.getOrElse(0.0)
-    	val prev = state.getOption.getOrElse(VisitState(0, 0.0))
-    	val current = prev + (1, delta)
-    	state.update(current)
-    	(key, current.avg)
-    	// the following return code is for debugging purpose and should chang the return value of the function from (String, Double) => (String)
-    	// current.toString
+
+    // define the maping function
+    // State[Object] will retain its value across different mini batch
+    def mappingFunc(key: String, value: Option[Double], state: State[MyState]): (String, Double) = {
+      val delta = value.getOrElse(0.0)
+      val prev = state.getOption.getOrElse(MyState(0, 0.0))
+      val current = prev + (1, delta)
+      state.update(current)
+      (key, current.avg)
+      // the following return code is for debugging purpose and should chang the return value of the function from (String, Double) => (String)
+      // current.toString
     }
 
 
